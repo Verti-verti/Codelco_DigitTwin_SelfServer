209f0a909c7cf7eff7e655f369bead52662e994c
Verti-verti <victoralip@gmail.com>
2025-07-16 18:07:12 -0400

    Fix: Add missing import for dynamic function

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-const Spline = dynamic(() => import('@splinetool/react-spline').then((mod) => mod.default), {
-      ssr: false,
-    });
+'use client';
 
-    export default function Home() {
-      return (
-        <main style={{ height: '100vh', width: '100vw' }}>
-          <Spline 
-            scene="/scene.splinecode" 
-          />
-        </main>
-      );
-    }
+// THIS IS THE MISSING LINE! You must import 'dynamic' to use it.
+import dynamic from 'next/dynamic';
+
+const Spline = dynamic(() => import('@splinetool/react-spline').then((mod) => mod.default), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <main style={{ height: '100vh', width: '100vw' }}>
+      <Spline
+        scene="/scene.splinecode"
+      />
+    </main>
+  );
+}
