6b8fa941eda275165af26a18687d8250d7d4b40e
Verti-verti <victoralip@gmail.com>
2025-07-16 18:39:39 -0400

    Refactor: Isolate Spline component in a wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@
 
 import dynamic from 'next/dynamic';
 
-// This is the correct, robust dynamic import for the real package
-const Spline = dynamic(
-  () => import('@splinetool/react-spline').then((mod) => mod.default),
-  {
-    ssr: false,
-  }
-);
+// Import our LOCAL wrapper component, not the package directly.
+const Spline = dynamic(() => import('@/components/SplineWrapper'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main style={{ height: '100vh', width: '100vw' }}>
       <Spline
-        // PASTE YOUR PUBLIC URL FROM SPLINE HERE
-        scene="https://my.spline.design/untitled-pGjVRYB1iz0Ee7eYRaJfbITH/scene.splinecode"
+        // Use your Public URL from Spline here
+        scene="https://prod.spline.design/i9f-OAU34uEWd9BF/scene.splinecode"
       />
     </main>
   );
---
+++ components/SplineWrapper.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+// This file's only job is to import and export the Spline component.
+import Spline from '@splinetool/react-spline';
+
+export default Spline;
